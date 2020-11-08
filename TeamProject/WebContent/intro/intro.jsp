16f3fa8820543a30d7edb149f648e93ef53bd163
hun9003 <65715992+hun9003@users.noreply.github.com>
2020-11-08 16:45:54 +0900

    header footer 수정

---
+++ TeamProject/WebContent/intro/intro.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>한줌에디터</title>
+<link href="css/default.css" rel="stylesheet" type="text/css">
+<link href="css/front.css" rel="stylesheet" type="text/css">
+<script src="js/jquery-3.5.1.js"></script>
+<script src="js/topMenu.js"></script>
+</head>
+<body>
+<div id="wrap">
+<jsp:include page="../inc/top.jsp"/>
+<main>
+
+</main>
+<jsp:include page="../inc/bottom.jsp"/>
+</div>
+</body>
+</html>
---
+++ TeamProject/src/controller/FrontIntro.java
@@ -0,0 +1,31 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/intro")
+public class FrontIntro extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String path = "intro/intro.jsp";
+		RequestDispatcher dispatcher = request.getRequestDispatcher(path);
+		dispatcher.forward(request, response);
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+}
